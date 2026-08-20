1a713ed3aaa95007d210db0ea9d8b74821753307
Franco-jav <f08208246@gmail.com>
2026-08-20 01:21:57 +0000

    Criação do Projeto

---
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Categoria.java
@@ -0,0 +1,16 @@
+package br.edu.unifio.ecommerce.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+
+public class Categoria {
+    private Short id;
+    private String nome;
+    private String descricao;
+
+}
