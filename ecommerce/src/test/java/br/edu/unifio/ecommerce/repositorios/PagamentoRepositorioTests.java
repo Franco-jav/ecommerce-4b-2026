c835cf3938069fd1e770505b8511dcefc3dfc213
Franco-jav <f08208246@gmail.com>
2026-09-15 22:57:22 +0000

    Adiciona repositórios e testes para as entidades Categoria, Cliente, ItemPedido, Pagamento, Pedido e Produto

---
+++ ecommerce/src/test/java/br/edu/unifio/ecommerce/repositorios/PagamentoRepositorioTests.java
@@ -0,0 +1,24 @@
+package br.edu.unifio.ecommerce.repositorios;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.edu.unifio.ecommerce.entidades.Pagamento;
+
+@SpringBootTest
+public class PagamentoRepositorioTests {
+    @Autowired
+    private PagamentoRepositorio pagamentoRepositorio;
+
+    @Test
+    public void deveBuscarPagamentoPorId() {
+        Pagamento pagamento = pagamentoRepositorio.findById(3).orElseThrow();
+
+        assertNotNull(pagamento);
+        assertEquals("APROVADO", pagamento.getStatus());
+    }
+}
